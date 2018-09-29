e1f8ca54f18125e613e9716a605dc83ad4ed47b3
Max <maryasych@gmail.com>
2018-09-29 13:07:10 +0300

    first progressbar: cange divident, second: validation input field oninput

--- resourses/meters.js
+++ resourses/meters.js
@@ -1,16 +1,19 @@
 let data = document.getElementsByName('personalData'),
 		cover = document.querySelector('.cover'),
-  	meter2 = document.querySelector('[role="progressbar2"]'),
+    meter = document.querySelectorAll('[role="progressbar"]')[1],
+    inputNum = document.querySelector('input[type="number"]'),
   	paintProgress = document.querySelector('.paintPogress');
 
 data.forEach(item =>{
   item.oninput = (() => {
   	let val = 0;
     data.forEach((item) => {
-    	val += item.value.length > 0 ? 33.333 : 0;
+      val += item.value.length > 0 ? 100/3 : 0;
     });
     cover.style.left = val + '%';
   });
 })
 
-paintProgress.onclick = (() => meter2.style.width = inputNum.value + '%')
+paintProgress.onclick = (() => {
+  inputNum.reportValidity() ? meter.style.width = inputNum.value + '%' : 0;
+})
